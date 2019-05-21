f445916dd3224aa8775b0bf1794f439dabd43658
Danie Martinoli <daniechris5@gmail.com>
2019-05-21 11:17:37 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 require "pry"
 def my_collect(array)
   empty_array = []
-  empty_array << array
   i = 0
   while i < array.length
-    yield array[i]
+    empty_array << yield(array[i])
     i = i + 1
   end
   empty_array
