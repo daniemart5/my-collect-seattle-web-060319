a7727e70b2cd2e2474474da98c9d129077ebd2c5
Danie Martinoli <daniechris5@gmail.com>
2019-05-21 10:23:48 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 require "pry"
 def my_collect(array)
-  empyt_array = []
+  empty_array = []
+  empty_array << array
   i = 0
   while i < array.length
     yield array[i]
+    array.upcase
     i = i + 1
-    empty_array << array.call(element)
   end
   empty_array
 end
